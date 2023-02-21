cf62b39506d7732e09e691b5c8ff858bcb7a4fd7
Ddayisme <122437344+Ddayisme@users.noreply.github.com>
2023-02-21 09:58:59 +0100

    post and get profile

--- src/components/pages/profile-page.jsx
+++ src/components/pages/profile-page.jsx
@@ -1,8 +1,52 @@
 import authGuard from "../authGuard";
 
+const apiURL = "https://ripe-eminent-moonflower.glitch.me/translations"
+const apiKEY= "noroff"
+const username= localStorage.getItem("username");
+
+const aGet=fetch(`${apiURL}/?username=${username}`)
+    .then(response => response.json())
+    .then(results => {
+       return results[0].translations;
+    })
+    .catch(error => {
+    })
+
+  
+
+function post(){
+        
+        fetch(`${apiURL}`, {
+        method: 'POST',
+        headers: {
+          'X-API-Key': apiKEY,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            username: username, 
+            translations: [] 
+        })
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Could not create new user')
+      }
+      return response.json()
+    })
+    .then(newUser => {
+      // newUser is the new user with an id
+    })
+    .catch(error => {
+    })
+}
+
+
+console.log()
+
 function profilePage(){
     return <div>
         <p>Profile page</p>
+        <p></p>
     </div>
 }
 
--- src/components/translation.jsx
+++ src/components/translation.jsx
@@ -1,4 +1,4 @@
-const IMG_PATH = "../LostInTranslation_Resources/individial_signs/"
+
 
 function Translation(props){
     let letters = String(props.props).split("");
